93a95e597a71febdfe94667b9d5debda105ece8f
deepthikreddy <deepthireddykesi@gmail.com>
2020-08-29 22:19:17 -0500

    New Companies

---
+++ New Companies.sql
@@ -0,0 +1,43 @@
+/* Amber's conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy: 
+
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+
+Note:
+
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+Input Format
+
+The following tables contain company data:
+
+Company: The company_code is the code of the company and founder is the founder of the company. 
+
+Lead_Manager: The lead_manager_code is the code of the lead manager, and the company_code is the code of the working company. 
+
+Senior_Manager: The senior_manager_code is the code of the senior manager, the lead_manager_code is the code of its lead manager, and the company_code is the code of the working company. 
+
+Manager: The manager_code is the code of the manager, the senior_manager_code is the code of its senior manager, the lead_manager_code is the code of its lead manager, and the company_code is the code of the working company. 
+
+Employee: The employee_code is the code of the employee, the manager_code is the code of its manager, the senior_manager_code is the code of its senior manager, the lead_manager_code is the code of its lead manager, and the company_code is the code of the working company. 
+
+*/
+
+
+--Query:
+
+
+SELECT
+a.company_code,
+b.founder,
+COUNT(DISTINCT(lead_manager_code)) AS lead_managers,
+COUNT(DISTINCT(senior_manager_code)) AS senior_managers,
+COUNT(DISTINCT(manager_code)) AS managers,
+COUNT(DISTINCT(employee_code)) AS employees
+FROM
+  employee a
+join                   -- To map only the similar company codes in both the tables
+  company b
+ON a.company_code = b.company_code 
+GROUP BY 1,2           -- To group aggregations
+ORDER BY 1             -- To order the data by ascending company code
+;
